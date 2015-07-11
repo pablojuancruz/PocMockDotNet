5aca1ec8c80af84cba0a9ecf8de76e0fff84eecd
Pablo Juan <4yoursystem@gmail.com>
2015-07-11 20:04:28 -0300

    Primeiro Commit

---
+++ ExemploConsultaCredito.Testes/Testes.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using ExemploConsultaCredito;
+
+namespace ExemploConsultaCredito.Testes
+{
+    [TestClass]
+    public class Testes
+    {
+        [TestMethod]
+        public void TestarParametroInvalido()
+        {
+            Mock<IServicoConsultaCredito> mock =
+                new Mock<IServicoConsultaCredito>();
+            mock.Setup(s => s.ConsultarPendenciasPorCPF(It.IsAny<string>()))
+                .Returns(() => null);
+
+            AnaliseCredito analise =
+                new AnaliseCredito(mock.Object);
+            StatusConsultaCredito status =
+                analise.ConsultarSituacaoCPF("123");
+
+            Assert.AreEqual(StatusConsultaCredito.ParametroEnvioInvalido,
+                status);
+        }
+
+        [TestMethod]
+        public void TestarErroComunicacao()
+        {
+            Mock<IServicoConsultaCredito> mock =
+                new Mock<IServicoConsultaCredito>();
+            mock.Setup(s => s.ConsultarPendenciasPorCPF(It.IsAny<string>()))
+                .Throws(new Exception("Teste Moq - Erro Comunicação"));
+
+            AnaliseCredito analise =
+                new AnaliseCredito(mock.Object);
+            StatusConsultaCredito status =
+                analise.ConsultarSituacaoCPF("12345678901");
+
+            Assert.AreEqual(StatusConsultaCredito.ErroComunicacao,
+                status);
+        }
+
+        [TestMethod]
+        public void TestarParametroSemPendencias()
+        {
+            Mock<IServicoConsultaCredito> mock =
+                new Mock<IServicoConsultaCredito>();
+            mock.Setup(s => s.ConsultarPendenciasPorCPF(It.IsAny<string>()))
+                .Returns(new List<Pendencia>());
+
+            AnaliseCredito analise =
+                new AnaliseCredito(mock.Object);
+            StatusConsultaCredito status =
+                analise.ConsultarSituacaoCPF("12345578901");
+
+            Assert.AreEqual(StatusConsultaCredito.SemPendencias,
+                status);
+        }
+
+        [TestMethod]
+        public void TestarInadimplente()
+        {
+            Mock<IServicoConsultaCredito> mock =
+                new Mock<IServicoConsultaCredito>();
+
+            var pendencias = new List<Pendencia>();
+            pendencias.Add(new Pendencia()
+                {
+                    CPF = "12345678901",
+                    DescricaoPendencia = "CARTÃO DE CRÉDITO"
+                });
+            
+            mock.Setup(s => s.ConsultarPendenciasPorCPF(It.IsAny<string>()))
+                .Returns(pendencias);
+
+            AnaliseCredito analise =
+                new AnaliseCredito(mock.Object);
+            StatusConsultaCredito status =
+                analise.ConsultarSituacaoCPF("12345578901");
+
+            Assert.AreEqual(StatusConsultaCredito.Inadimplente,
+                status);
+        }
+    }
+}
---
+++ ExemploConsultaCredito/AnaliseCredito.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace ExemploConsultaCredito
+{
+    public class AnaliseCredito
+    {
+        private IServicoConsultaCredito _servico;
+
+        public AnaliseCredito(IServicoConsultaCredito servConsultaCredito)
+        {
+            this._servico = servConsultaCredito;
+        }
+
+        public StatusConsultaCredito ConsultarSituacaoCPF(string cpf)
+        {
+            try
+            {
+                var pendencias = _servico
+                    .ConsultarPendenciasPorCPF(cpf);
+
+                if (pendencias == null)
+                    return StatusConsultaCredito.ParametroEnvioInvalido;
+                else if (pendencias.Count == 0)
+                    return StatusConsultaCredito.SemPendencias;
+                else
+                    return StatusConsultaCredito.Inadimplente;
+            }
+            catch
+            {
+                return StatusConsultaCredito.ErroComunicacao;
+            }
+        }
+    }
+}
